8ab10a3c17b80955a61b04082a996da00470dafb
mbusso <mbusso@hexacta.com>
2017-08-02 03:55:32 +0000

    remove comment

--- server/api/thing/index.js
+++ server/api/thing/index.js
@@ -9,8 +9,6 @@ var auth = require('../../auth/auth.service');
 router.get('/', controller.index);
 router.get('/:id', controller.show);
 router.post('/', auth.isAuthenticated(), controller.create);
-// router.put('/:id', auth.isAuthenticated(), controller.update);
-// router.patch('/:id', auth.isAuthenticated(), controller.update);
 router.delete('/:id', auth.isAuthenticated(), controller.destroy);
 
 router.put('/:id/star', auth.isAuthenticated(), controller.star);
