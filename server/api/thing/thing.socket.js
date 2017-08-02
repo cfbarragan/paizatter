05f601e7857c5e525e5345089169b3485d3459fb
mbusso <mbusso@hexacta.com>
2017-08-02 04:00:19 +0000

    ejercicio 4

--- server/api/thing/thing.socket.js
+++ server/api/thing/thing.socket.js
@@ -9,18 +9,10 @@ var thing = require('./thing.model');
 exports.register = function(socket) {
   thing.schema.post('save', function (doc) {
     onSave(socket, doc);
-  });
-  thing.schema.post('remove', function (doc) {
-    onRemove(socket, doc);
-  });
+  });  
 }
 
 function onSave(socket, doc, cb) {
-  doc.populate('user', 'name', function(err){
-	  socket.emit('thing:save', doc);
-  })
+  socket.emit('thing:save', doc);
 }
 
-function onRemove(socket, doc, cb) {
-  socket.emit('thing:remove', doc);
-}
